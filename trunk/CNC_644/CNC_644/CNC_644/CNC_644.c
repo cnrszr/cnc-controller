6ddd43356613e95cee88eeaf97563a99affca322
seblovett@gmail.com <seblovett@gmail.com@0db423d8-ab52-a23a-0e37-41f864d3acc2>
2012-07-18 22:08:32 +0000

    Started using Atmel studio 6.
    Added extra commands to the command processor.

--- trunk/CNC_644/CNC_644/CNC_644/CNC_644.c
+++ trunk/CNC_644/CNC_644/CNC_644/CNC_644.c
@@ -18,9 +18,12 @@ unsigned char str[] = {"Command Received"};
 int main(void)
 {
 	USART_Init(MYUBBR); //initialise UART0
+	uart_SendString("Set Up...\n\r");
 	speed_cntr_Init_Timer1();
 	sm_driver_Init_IO();
-	
+	XLocation = 0;
+	YLocation = 0;
+	ZLocation = 0;
 	sei();
 	DDRA = 0xFF;//set port c to outputs
 	PORTA = 0x00;
@@ -36,79 +39,101 @@ int main(void)
 	accel = 71;
 	speed = 12;
 	decel = 71;
+	uart_SendString("Ready!\n\r");
 	while(1)
 	{
 		if(COMMANDRECEIVED == TRUE)
-		{
-			if(Received[0] == 'G')
+		{			
+			switch(Received[0])
 			{
-				if(statusX.running == TRUE)
-				{
-					StartX();
-				}
+				case 'g': //start all set up motors. 
+					if(statusX.running == TRUE)
+					{
+						StartX();
+					}
 				
-				if(statusY.running == TRUE)
-				{
-					StartY();
-				}
+					if(statusY.running == TRUE)
+					{
+						StartY();
+					}
 				
-				if(statusZ.running == TRUE)
-				{
-					StartZ();
-				}
-							
-			}
-			else if (Received[0] == 'X')
-			{
-				speed_cntr_SetupX(Step, accel, decel, speed);
-			}
-			else if (Received[0] == 'Y')
-			{
-				speed_cntr_SetupY(Step, accel, decel, speed);
-			}
-			else if (Received[0] == 'Z')
-			{
-				speed_cntr_SetupZ(Step, accel, decel, speed);
-			}
-			else if(Received[0] == 'S')//set steps
-			{
-				Step = atoi((char const *)Received+2);	
-				//speed_cntr1_Setup1(Step, accel, decel, speed);
-			}
-			else if (Received[0] == 'A')//set acceleration
-			{
-				accel = atoi((char const *)Received+2);
-				//speed_cntr1_Setup1(Step, accel, decel, speed);
-			}
-			else if (Received[0] == 'd')//set deceleration
-			{
-				decel = atoi((char const *)Received+2);
-				//speed_cntr1_Setup1(Step, accel, decel, speed);
-			}
-			else if (Received[0] == 's')//set speed
-			{
-				speed = atoi((char const *)Received+2);
-				//speed_cntr1_Setup1(Step, accel, decel, speed);
-				if (speed > MAXSPEED)
-				{
-					uart_SendString("WARNING: EXCEEDED MAXIMUM SPEED");
-				}
-			}
-			else if (Received[0] == 'I')
-			{
-				uart_SendString("\n\rSteps: ");
-				uart_SendInt(Step);
-				uart_SendString("\n\rAccel: ");
-				uart_SendInt(accel);
-				uart_SendString("\n\rSpeed: ");
-				uart_SendInt(speed);
-				uart_SendString("\n\rDecel: ");
-				uart_SendInt(decel);
+					if(statusZ.running == TRUE)
+					{
+						StartZ();
+					}
+					break;
 				
-			}
-			else
-			{
-				uart_SendString("Incorrect Command");
+				case 'X': //Set up the X motor with current settings
+					speed_cntr_SetupX(Step, accel, decel, speed);
+					break;
+					
+				case 'Y': //set up the Y motor with current settings
+					speed_cntr_SetupY(Step, accel, decel, speed);
+					break;
+					
+				case 'Z': //set up the Z motor with current settings
+					speed_cntr_SetupZ(Step, accel, decel, speed);
+					break;
+					
+				case 'S'://set the Step variable
+					Step = atoi((char const *)Received+2);
+					break;
+					
+				case 'a'://set the Accel Variable
+					accel = atoi((char const *)Received+2);
+					break;
+					
+				case 'd'://set up the deceleration variable
+					decel = atoi((char const *)Received+2);
+					break;
+					
+				case 's'://set the steps variable
+					speed = atoi((char const *)Received+2);
+					//speed_cntr1_Setup1(Step, accel, decel, speed);
+					if (speed > MAXSPEED)
+					{
+						uart_SendString("WARNING: EXCEEDED MAXIMUM SPEED");
+					}
+					break;
+					
+				case 'I'://show all variable information
+					uart_SendString("\n\rSteps: ");
+					uart_SendInt(Step);
+					uart_SendString("\n\rAccel: ");
+					uart_SendInt(accel);
+					uart_SendString("\n\rSpeed: ");
+					uart_SendInt(speed);
+					uart_SendString("\n\rDecel: ");
+					uart_SendInt(decel);
+					break;
+					
+				case 'L'://show current locations of device
+					USART_Transmit('X');
+					USART_Transmit('=');
+					uart_SendInt(XLocation);
+					USART_Transmit('\r');
+					USART_Transmit('\n');
+					USART_Transmit('Y');
+					USART_Transmit('=');
+					uart_SendInt(YLocation);
+					USART_Transmit('\r');
+					USART_Transmit('\n');
+					USART_Transmit('Z');
+					USART_Transmit('=');
+					uart_SendInt(ZLocation);
+					USART_Transmit('\r');
+					USART_Transmit('\n');
+					break;
+					
+				case '0'://Zero all Variables.
+					XLocation = 0;
+					YLocation = 0;
+					ZLocation = 0;
+					break;	
+					
+				default://unsupported command
+					uart_SendString("Incorrect Command");
+					break;
 			}
 			COMMANDRECEIVED = FALSE;
 			FlushBuffer();
@@ -116,7 +141,7 @@ int main(void)
 		}
 		else
 		{
-			PORTA = PORTA;//added to fill the else statement else the compiler removes the entire if/else
+			PORTA = PORTA;
 		}
 	}
 }
